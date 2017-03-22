30bf79d1a75a69d2e1c8f132fe4261e4529523a3
amazzenga <7amazzenga@stpatrick.org>
2017-03-22 14:01:26 -0500

    finished click

--- click.js
+++ click.js
@@ -7,6 +7,7 @@ function samecolor(){
   divs[1].style.backgroundColor = "#556699";
   divs[2].style.backgroundColor = "#556699";
   divs[3].style.backgroundColor = "#556699";
+
   divs[0].style.opacity = 0.3;
   divs[1].style.opacity = 0.5;
   divs[2].style.opacity = 0.7;
@@ -16,4 +17,13 @@ function samecolor(){
 function diffcolor(){
   let doc = document;
   let divs2 = doc.querySelectorAll("#diffcolor div");
+
+  divs2[0].style.backgroundColor = "#AABB66";
+  divs2[1].style.backgroundColor = "#556699";
+  divs2[2].style.backgroundColor = "#AABB66";
+  divs2[3].style.backgroundColor = "#556699";
+  divs2[4].style.backgroundColor = "#AABB66";
+  divs2[5].style.backgroundColor = "#556699";
+  divs2[6].style.backgroundColor = "#AABB66";
+  divs2[7].style.backgroundColor = "#556699";
 }
