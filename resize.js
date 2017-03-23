60b7103887752dec9c8fa2bbfcbd981195476a45
amazzenga <7amazzenga@stpatrick.org>
2017-03-23 13:55:45 -0500

    finished

--- resize.js
+++ resize.js
@@ -1,7 +1,25 @@
 //Declare tabletSize() here
 function tabletSize(){
   let doc = document;
-  let all = doc.querySelectorAll("#")
+  let all = doc.querySelectorAll("section");
+  let width = all.style.width;
+  let height = all.style.height;
+
+  all[0].style.width = String(width*0.75)+"px";
+  all[0].style.height = String(height*0.75)+"px";
+  all[1].style.width = String(width*0.75)+"px";
+  all[1].style.width = String(height*0.75)+"px";
 }
 
 //Declare phoneSize() here
+function phoneSize(){
+  let doc = document;
+  let all = doc.querySelectorAll("section");
+  let width = section.width;
+  let height = section.height;
+
+  all[0].style.width = String(width*0.5)+"px";
+  all[0].style.height = String(height*0.5)+"px";
+  all[1].style.width = String(width*0.5)+"px";
+  all[1].style.width = String(height*0.5)+"px";
+}
